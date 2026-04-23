0787a50e5062f4efef6cc003a5d3b24aa37e5cb7
Naman Shrivastava <namanshrivastava008@gmail.com>
2026-04-23 16:29:36 +0530

    feat: add support for MistralAI and Cohere models; update Google model version

--- src/services/models.service.ts
+++ src/services/models.service.ts
@@ -1,7 +1,23 @@
 import { ChatGoogle } from "@langchain/google";
+import { ChatMistralAI } from "@langchain/mistralai";
+import { ChatCohere } from "@langchain/cohere";
 import config from "../config/config.js";
 
-const model = new ChatGoogle({
+export const model = new ChatGoogle({
   model: "gemini-flash-latest",
   apiKey: config.GOOGLE_API_KEY,
 });
+
+export const getMistralModel = (): any => {
+  return new ChatMistralAI({
+    model: "mistral-medium-latest",
+    apiKey: config.MISTRAL_API_KEY,
+  });
+};
+
+export const getCohereModel = (): any => {
+  return new ChatCohere({
+    model: "command-r-08-2024",
+    apiKey: config.COHERE_API_KEY,
+  });
+};
